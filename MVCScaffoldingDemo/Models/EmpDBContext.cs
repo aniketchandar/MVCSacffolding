89d2fe00cbb2707238bcb61adaa2ba469bd2dd2b
Aniket <aniketchandar01@gmail.com>
2019-09-04 13:33:32 +0530

    Commiit initial

---
+++ MVCScaffoldingDemo/Models/EmpDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCScaffoldingDemo.Models
+{
+    public class EmpDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+
